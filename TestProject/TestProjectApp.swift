ba475396553169757b9a6df25f36c1db374f3aae
Rawann0m <raawannm@gmail.com>
2025-03-25 11:17:43 +0300

    Initial Commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Rawan on 25/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
